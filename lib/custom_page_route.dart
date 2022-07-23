a6682f07a69a40c6911bb13807d3d413da51d24c
rahulshahare <rahulshahare@gmail.com>
2022-07-23 19:04:09 +0530

    Creating Sliding animation like React Native

--- lib/custom_page_route.dart
+++ lib/custom_page_route.dart
@@ -6,12 +6,19 @@ class CustomPageRoute extends PageRouteBuilder {
   CustomPageRoute({
     required this.child,
   }) : super(
-          transitionDuration: Duration(seconds: 1),
+          //transitionDuration: Duration(seconds: 1),
           pageBuilder: (context, animation, secondaryAnimation) => child,
         );
 
   @override
   Widget buildTransitions(BuildContext context, Animation<double> animation,
           Animation<double> secondaryAnimation, Widget child) =>
-      ScaleTransition(scale: animation, child: child);
+      //ScaleTransition(scale: animation, child: child);
+      SlideTransition(
+        position: Tween<Offset>(
+          begin: Offset(1, 0),
+          end: Offset.zero,
+        ).animate(animation),
+        child: child,
+      );
 }
