a56a1303c24fea59dac78f76c51c9e6c0e9c1eab
rahulshahare <rahulshahare@gmail.com>
2022-07-24 02:04:03 +0530

    tiktik

--- lib/main.dart
+++ lib/main.dart
@@ -83,6 +83,16 @@ class RoutesExample extends StatelessWidget {
               },
               child: const Text('Call page two from #RIGHT'),
             ),
+            const SizedBox(height: 15),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).push(CustomPageRoute(
+                  child: Tiktik(),
+                  direction: AxisDirection.right,
+                ));
+              },
+              child: const Text('TIktik'),
+            ),
             const SizedBox(height: 40),
             const Text("Rahul shahare", textAlign: TextAlign.center),
             const Text("Founder @ Oceangreen Technology",
@@ -118,6 +128,42 @@ class SecondPage extends StatelessWidget {
             ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class Tiktik extends StatelessWidget {
+  const Tiktik({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          // Top section
+          Container(
+            height: 100.0,
+            padding: EdgeInsets.only(bottom: 15.0),
+            color: Colors.yellow[300],
+          ),
+
+          // Middle expanded
+          Expanded(
+              child: Row(
+                  mainAxisSize: MainAxisSize.max,
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: <Widget>[
+                Expanded(child: Container(color: Colors.green[300])),
+                Container(
+                  width: 100.0,
+                  color: Colors.red[300],
+                )
+              ])),
+
+          // Bottom Section
+          Container(height: 80.0, color: Colors.blue[300]),
+        ],
       ),
     );
   }
